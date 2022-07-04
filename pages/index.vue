444ab309b19dea314b8cee4afc29c7b33efd8b3b
nyohoo <nyohoo2013@gmail.com>
2022-07-04 12:54:42 +0900

    add: index,resultページ

--- pages/index.vue
+++ pages/index.vue
@@ -1,83 +1,97 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
+  <v-container>
+    <v-row>
+      <v-text-field
+        prepend-icon="mdi-magnify-plus-outline"
+        type="search"
+        label="アルバム検索"
+        v-model="query"
+        solo
+        height="30"
+        rounded
+        @input="getSearch"
+        class="mt-4"
+        id="searchField"
+        hint="アルバム名またはアーティスト名を入力"
+      />
+    </v-row>
+    <v-row>
+      <v-col v-for="result in results" :key="result.id" cols="6" sm="3">
+        <v-card height="100%" class="black--text white">
+          <v-img
+            :src="'http://image.tmdb.org/t/p/w300/' + result.poster_path"
+          />
+          <v-card-title class="justify-center text-body-1 text-sm-h6">{{
+            result.title
+          }}</v-card-title>
+          <v-card-subtitle
+            class="black--text justify-center text-caption text-sm-body-2"
           >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+            {{ result.release_date }}
+          </v-card-subtitle>
+          <v-card-actions class="justify-center">
+            <v-btn
+              v-if="checkSelectMovie(result)"
+              @click="removeMovies(result)"
+            >
+              <v-icon>mdi-minus</v-icon>
+            </v-btn>
+            <v-btn v-else @click="pushMovies(result)">
+              <v-icon>mdi-plus</v-icon>
+            </v-btn>
+          </v-card-actions>
+          <v-card-text v-if="toggle" class="black--text">
+            {{ result.overview }}
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
-}
+  asyncData({ $config: { apiKey } }) {
+    return { apiKey };
+  },
+  name: "TopIndex",
+  data() {
+    return {
+      query: "",
+      results: [],
+    };
+  },
+  computed: {
+    movies() {
+      return this.$store.state.movies.movies;
+    },
+  },
+  mounted() {
+    document.getElementById("searchField").focus();
+  },
+  methods: {
+    getSearch() {
+      this.$axios
+        .get("https://api.themoviedb.org/3/search/movie", {
+          params: { api_key: this.apiKey, query: this.query, language: "ja" },
+        })
+        .then((response) => {
+          this.results = response.data.results;
+        })
+        .catch(() => {
+          location.reload();
+        });
+    },
+    pushMovies(movie) {
+      if (this.movies.length < 5)
+        this.$store.dispatch("movies/addMovies", movie);
+    },
+    removeMovies(movie) {
+      this.$store.dispatch("movies/deleteMovies", movie);
+    },
+    checkSelectMovie(result) {
+      return this.movies.some((movie) => movie.id === result.id);
+    },
+  },
+};
 </script>
