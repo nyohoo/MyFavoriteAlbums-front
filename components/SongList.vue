ccd4ce195b591142b9554195a3645d2e982cc9ec
nyohoo <nyohoo2013@gmail.com>
2022-08-01 23:04:08 +0900

    fix: iframeの呼び出しを修正、各種ホバーの設定

--- components/SongList.vue
+++ components/SongList.vue
@@ -8,40 +8,58 @@
           tile 
           flat
         >
-          <v-img
-            :src="result.images[0].url"
-            :lazy-src="result.images[0].url"
-            :alt="result.name"
-            aspect-ratio="1"
-            @click="openIframe"
-            class="ma-2 mb-0 rounded-0"
-          >
-            <!-- ローディング中の処理 -->
-            <template v-slot:placeholder>
-              <v-row
-                class="fill-height ma-0"
-                align="center"
-                justify="center"
-              >
-                <v-progress-circular
-                  indeterminate
-                  color="green lighten-3"
-                ></v-progress-circular>
-              </v-row>
-            </template>
-          </v-img>
+          <v-hover v-slot:default="{ hover }">
+            <v-img
+              :src="result.images[0].url"
+              :lazy-src="result.images[0].url"
+              :alt="result.name"
+              :class="{ 'pointer': hover }"
+              :style="{ 'cursor': hover ? 'pointer' : 'default' }"
+              aspect-ratio="1"
+              @click="openAlbumIframe(result)"
+              class="ma-2 mb-0 rounded-0"
+            >
+              <!-- ローディング中の処理 -->
+              <template v-slot:placeholder>
+                <v-row
+                  class="fill-height ma-0"
+                  align="center"
+                  justify="center"
+                >
+                  <v-progress-circular
+                    indeterminate
+                    color="green lighten-3"
+                  ></v-progress-circular>
+                </v-row>
+              </template>
+            </v-img>
+          </v-hover>
         </v-card>
         <v-card-text class="pt-3 pb-0">
-          <p class="body-2 font-weight-bold ma-0 text-truncate white--text">
-            {{ result.name }}
-          </p>
-          <p class="caption font-weight-light mb-0 text-truncate">
-            {{ result.artists[0].name }}
-          </p>
+          <v-hover v-slot:default="{ hover }">
+            <p 
+              class="font-weight-bold ma-0 text-truncate"
+              :style="{ 'color': hover ? '#008000' : '#FFF' }"
+              @click="openAlbumIframe(result)"
+              >
+              {{ result.name }}
+            </p>
+          </v-hover>
+          <!-- ホバー可能にする -->
+          <v-hover v-slot:default="{ hover }">
+            <p 
+              class="caption font-weight-light mb-0 text-truncate" 
+              :class="{ 'text-caption': hover }"
+              :style="{ 'color': hover ? '#008000' : '' }"
+              @click="openArtistIframe(result)"
+            >
+              {{ result.artists[0].name }}
+            </p>
+          </v-hover>
         </v-card-text>
         <v-card-actions>
           <v-btn
-            @click="openIframe"
+            @click="openAlbumIframe(result)"
             class="ml-0"
             fab
             icon
@@ -51,10 +69,11 @@
           >
             <v-icon color="secondary">mdi-play-circle</v-icon>
             <Iframe
-              :isDisplay="iframe"
+              :isIframe="iframe"
               :isSongList="isSongList"
-              :song="result.id"
-              @closeDialog="iframe = false, isSongList = false" 
+              :spotifyId="spotifyId"
+              :embedType="embedType"
+              @closeDialog="iframe = false, isSongList = false, embedType = ''" 
             />
           </v-btn>
           <v-btn
@@ -100,6 +119,8 @@ export default {
     return {
       iframe: false,
       isSongList: false,
+      spotifyId: '',
+      embedType: '',
     };
   },
   props: {
@@ -117,9 +138,17 @@ export default {
     },
   },
   methods: {
-    openIframe() {
+    openAlbumIframe({ id }) {
       this.iframe = true
       this.isSongList = true
+      this.embedType = 'album'
+      this.spotifyId = id
+    },
+    openArtistIframe(result) {
+      this.iframe = true
+      this.isSongList = false
+      this.embedType = 'artist'
+      this.spotifyId = result.artists[0].id
     },
     pushAlbums(album) {
       this.$store.dispatch("albums/addAlbums", album);
