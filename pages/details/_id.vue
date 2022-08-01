6ad6a7328df7a1afbcbfe7881d06d04da00b89fa
nyohoo <nyohoo2013@gmail.com>
2022-08-01 23:06:11 +0900

    add: 投稿詳細ページ作成

--- pages/details/_id.vue
+++ pages/details/_id.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-container>
-    <!-- 中央寄せ -->
+    
+  <v-container fluid>
     <v-container mt-6 mb-13>
       <v-row algin="center" justify="center">
         <div align="left">
@@ -15,169 +15,139 @@
       <v-row algin="center" justify="center" class="mt-0 mb-5">
         <div v-text="post.hash_tag" class="text-h4" />
       </v-row>
-      <v-row algin="center" justify="center">
-        <v-card height="45%" width="45%">
-          <v-img
-            :src="post.image"
-            :lazy-src="post.image"
-            aspect-ratio="1"
-            class="rounded-0"
-          >
-            <template v-slot:placeholder>
-              <v-row class="fill-height ma-0" align="center" justify="center">
-                <v-progress-circular
-                  indeterminate
-                  color="grey lighten-5"
-                ></v-progress-circular>
-              </v-row>
-            </template>
-          </v-img>
+      <!-- spotifyのアルバム情報から9枚を描画 -->
+      <v-row justify="center">
+        <v-card height="50vh" width="50vh" justify="center">      
+          <v-container>
+            <v-row> 
+              <v-col
+                v-for="album in post.albums"
+                :key="album.id"
+                class="d-flex child-flex ma-0 pa-0"
+                cols="4"
+              >
+                <!-- アルバム画像の表示 -->
+                <v-hover v-slot:default="{ hover }">
+                  <v-img
+                    :src="album.images[0].url"
+                    :lazy-src="album.images[0].url"
+                    :alt="album.name"
+                    aspect-ratio="1"
+                    class="my-0 rounded-0"
+                    :class="{ 'pointer': hover }"
+                    :style="{ 'cursor': hover ? 'pointer' : 'default' }"
+                    @click="openAlbumIframe(album)"
+                  >
+                    <!-- ローディング中の処理 -->
+                    <template v-slot:placeholder>
+                      <v-row
+                        class="fill-height ma-0"
+                        align="center"
+                        justify="center"
+                      >
+                        <v-progress-circular
+                          indeterminate
+                          color="grey lighten-5"
+                        ></v-progress-circular>
+                      </v-row>
+                    </template>
+                  </v-img>
+                </v-hover>
+              </v-col>
+              <!-- iframeの呼び出し -->
+              <Iframe 
+                :isIframe="iframe" 
+                :spotifyId="spotifyId" 
+                :embedType="embedType" 
+                @closeDialog="iframe = false, embedType = ''" 
+              />
+            </v-row>
+          </v-container>
         </v-card>
       </v-row>
     </v-container>
 
     <!-- 中央寄せ -->
-    <v-container justify="center" alignt="center">
-      <v-row algin="center" justify="center">
-        <v-col cols="6" xs="12" justify="center">
-          <v-list>
+    <v-container justify="center">
+      <v-row justify="center">
+        <v-col cols="12" sm="6" justify="center">
+          <v-list nav rounded>
             <template v-for="(album, index) in post.albums">
-              <v-header class="pl-5">
-                <!-- <span>{{ index + 1 }}</span> -->
-                <!-- <span ml-0 pl-0>. </span> -->
-                <span class="text-h6">{{ album.name }}</span>
-              </v-header>
-
-              <v-list-item selectable two-line>
-                <v-list-item-avatar tile height="30%" width="30%">
-                  <v-img :src="album.images[0].url" />
+              <!-- <v-header class="mb-0 pb-0">
+                <span class="text-h9 font-weight-bold pl-4">{{ album.name }}</span>
+              </v-header> -->
+              <v-list-item dense>
+                <v-list-item-avatar tile height="40%" width="40%" class="mr-2">
+                  <v-hover v-slot:default="{ hover }">
+                    <v-img
+                      :src="album.images[0].url"
+                      :class="{ 'pointer': hover }"
+                      :style="{ 'cursor': hover ? 'pointer' : 'default' }"
+                      @click="openAlbumIframe(album)"
+                    />
+                  </v-hover>
                 </v-list-item-avatar>
-                <!-- <v-divider vertical class="my-4"></v-divider> -->
-                <v-list-item-content>
-                  <!-- <v-list-item-title 
-                    class="ml-4 mt-0 pt-0"
+                <v-list-item-content class="my-0">
+                  <v-list-item-title 
+                    class="ml-0 my-0 py-0 wrap-text"
                   >
-                    <span class="text-h2xl leading-7 font-semibold">{{ album.name }}</span>
-                  </v-list-item-title> -->
-
-                  <span class="caption text--secondary ml-4">{{
-                    post.dates[index]
-                  }}</span>
-                  <div class="mt-4 mb-0"></div>
-
+                    <v-hover v-slot="{ hover }">
+                      <p 
+                        class="text-body-2 font-weight-bold my-0 py-0 ml-1"
+                        :class="{ 'text-body-2': hover }"
+                        :style="{ 'color': hover ? '#008000' : '' }"
+                        @click="openAlbumIframe(album)"
+                        >
+                        {{ album.name }}
+                      </p>
+                    </v-hover>
+                  </v-list-item-title>
+                  <span class="text-caption font-weight-thin ml-1 pt-1">
+                    ({{ post.dates[index] }})
+                  </span>
                   <v-list-item-title
-                    v-text="album.artists[0].name"
-                    class="ml-4 mt-1 pt-0"
-                  ></v-list-item-title>
+                    class="ml-1 mt-3"
+                  >
+                    <v-hover v-slot="{ hover }">
+                      <p 
+                        class="text-caption font-weight-medium wrap-text" 
+                        :class="{ 'text-caption': hover }"
+                        :style="{ 'color': hover ? '#008000' : '' }"
+                        @click="openArtistIframe(album)"
+                      >
+                        {{ album.artists[0].name }}
+                      </p>
+                    </v-hover>
+                  </v-list-item-title>
+                  
                 </v-list-item-content>
               </v-list-item>
-              <v-divider v-if="index !== 8" class="mt-7 mb-3"></v-divider>
+              
+              <v-row class="ml-4 mt-1 mb-1 mr-0">
+                <div id="songlink">
+                  <iframe 
+                    :src="`https://embed.odesli.co/?url=spotify:album:${album.id}&theme=dark`" 
+                    frameborder="0"
+                    allowtransparency
+                    allowfullscreen
+                    sandbox="allow-same-origin allow-scripts allow-presentation allow-popups allow-popups-to-escape-sandbox">
+                  </iframe>
+                </div>
+              </v-row>
+              <v-divider v-if="index !== 8" class="mt-5 mb-3 mx-4"></v-divider>
             </template>
           </v-list>
         </v-col>
       </v-row>
+
+
     </v-container>
-
-    <!-- <v-container
-      v-for="album in post.albums"
-      :key="album.id"
-      ma-3 
-      justify="center"
-      align="center"
-    >
-      <v-row no-gutters>
-        <v-col cols="3" >
-          <v-img
-            :src="album.images[0].url"
-            :lazy-src="album.images[0].url"
-            :alt="album.name"
-            aspect-ratio="1"
-            height="100%"
-            width="100%"
-            class="ma-2 mb-0 rounded-0"
-          >
-
-            <template v-slot:placeholder>
-              <v-row
-                class="fill-height ma-0"
-                align="center"
-                justify="center"
-              >
-                <v-progress-circular
-                  indeterminate
-                  color="green lighten-3"
-                ></v-progress-circular>
-              </v-row>
-            </template>
-          </v-img>
-        </v-col>
-      
-        <v-col class="3">
-          <v-card-text class="pt-3 pb-0">
-            <p class="body-2 font-weight-bold ma-0 text-truncate white--text">
-              {{ album.name }}
-            </p>
-            <p class="caption font-weight-light mb-0 text-truncate">
-              {{ album.artists[0].name }}
-            </p>
-          </v-card-text>
-        </v-col>
-      </v-row>
-
-      <v-divider
-      ></v-divider>
-
-    </v-container> -->
-
-    <!-- <v-col
-        v-for="album in post.albums"
-        :key="album.id"
-        ma-3 
-        justify="center"
-        align="center"
-      >
-        <v-card
-          flat
-          class="pa-0"
-          height="100%"
-          width="75%"
-        >
-          <div class="d-flex flex-no-wrap">
- 
-              <v-img 
-                :src="album.images[0].url"
-                width="20%"
-                height="20%"
-                ></v-img>
-            
-            <div>
-              <v-card-title
-                class="text-h6"
-                align="left"
-                v-text="album.name"
-              ></v-card-title>
-              <v-card-subtitle v-text="album.artists[0].name" align="left" class="text-h7 caption"></v-card-subtitle>
-              <v-card-actions>
-                  <v-btn
-                    class="ml-2 mt-3"
-                    fab
-                    icon
-                    height="40px"
-                    right
-                    width="40px"
-                  >
-                    <v-icon>mdi-play</v-icon>
-                  </v-btn>
-              </v-card-actions>
-            </div>
-          </div>
-        </v-card>
-      </v-col> -->
   </v-container>
 </template>
 
 <script>
 import { axios } from "@/plugins/axios";
+import Iframe from '@/components/Iframe.vue'
 
 export default {
   async asyncData(context) {
@@ -190,5 +160,51 @@ export default {
       titleTemplate: "",
     };
   },
+  components: {
+    Iframe,
+  },
+  data() {
+    return {
+      // loading: true,
+      iframe: false,
+      detail: false,
+      embedType: "",
+      spotifyId: "",
+    };
+  },
+  methods: {
+    openAlbumIframe({ id }) {
+      this.iframe = true;
+      this.detail = true;
+      this.embedType = "album";
+      this.spotifyId = id;
+    },
+      openArtistIframe(album) {
+      this.iframe = true;
+      this.detail = true;
+      this.embedType = "artist";
+      this.spotifyId = album.artists[0].id;
+    },
+  },
 };
 </script>
+
+<style>
+.wrap-text {
+  word-break: break-all;
+  white-space: normal;
+}
+
+#songlink {
+  width: 105%;
+  height: 35px;
+  overflow: hidden;
+}
+
+#songlink iframe {
+  width: 105%;
+  height: 35px;
+  margin-left: -50px;
+}
+
+</style>
