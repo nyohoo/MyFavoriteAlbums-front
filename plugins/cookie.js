0d6eba0d000b3da877b6fe07e5e608b4a2da799a
nyohoo <nyohoo2013@gmail.com>
2022-08-03 03:58:34 +0900

    fix: console,log削除、不要なファイル削除

--- plugins/cookie.js
+++ plugins/cookie.js
@@ -1,13 +1,7 @@
 export default ({ store, app, $axios }) => {
   const session = app.$cookies.get('session')
-  console.log(session)
-  console.log("ページリロード処理")
   if (session) {
     store.commit('login/user', session.user)
     store.commit('login/auth', session.tokens)
-    console.log("store/login/の中身")
-    console.log(store.state.login.user)
-    console.log(store.state.login.auth)
-    console.log("store/login/の中身ここまで")
   }
 }
