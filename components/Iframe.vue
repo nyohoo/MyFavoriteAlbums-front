d445bc4bb992fca32fcb4c27ac8dc7939216b536
nyohoo <nyohoo2013@gmail.com>
2022-08-07 09:03:12 +0900

    fix: iframeの不要なオプションを削除

--- components/Iframe.vue
+++ components/Iframe.vue
@@ -50,7 +50,6 @@
         width="100%" 
         height="380" 
         frameBorder="0" 
-        allowfullscreen="" 
         allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
       >
       </iframe>
