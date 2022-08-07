a23e4e4071f066b4993da37e6a8bc52dfca1fa2f
nyohoo <nyohoo2013@gmail.com>
2022-08-07 09:03:34 +0900

    add: ツイッター作成中のローディングを追加

--- components/Loading.vue
+++ components/Loading.vue
@@ -4,9 +4,8 @@
     v-if="loading"
     fullscreen
     full-width
-  >
-    
-    <v-container fluid fill-height width="100%" style="background-color: rgba(110, 159, 130, 0.9);">
+  > 
+    <v-container fluid fill-height width="100%" style="background-color: rgba(150, 150, 150, 0.85);">
       <v-container>
         <v-row class="mb-12" justify="center">
           <v-progress-circular
@@ -18,8 +17,9 @@
           </v-progress-circular>
         </v-row>
         <v-row  justify="center">
-          <div>
-            <p class="black--text text-h4 font-weight-bold"><v-icon>mdi-robot</v-icon>画像作成中です...<v-icon>mdi-robot</v-icon></p>
+          <div >
+            <p v-if="isCreatePost" class="black--text text-h4 font-weight-bold"><v-icon left large color="light-green lighten-1">mdi-robot</v-icon>画像作成中です...<v-icon right large color="light-green lighten-1">mdi-robot</v-icon></p>
+            <p v-if="isShareTwitter" class="black--text text-h4 font-weight-bold"><v-icon left large color="light-green lighten-1">mdi-robot</v-icon>ツイッターに投稿中です...<v-icon right large color="light-green lighten-1">mdi-robot</v-icon></p>
           </div>
         </v-row>
         <v-row  justify="center">
@@ -36,6 +36,14 @@
 export default {
   props: {
     loading: {
+      type: Boolean,
+      default: false
+    },
+    isCreatePost: {
+      type: Boolean,
+      default: false
+    },
+    isShareTwitter: {
       type: Boolean,
       default: false
     },
