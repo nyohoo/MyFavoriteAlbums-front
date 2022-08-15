efadc0e823b1c88f0e3bb2eb7a93372c438d1ad7
nyohoo <nyohoo2013@gmail.com>
2022-08-16 02:36:41 +0900

    add: ユーザーのいいねとポストを子コンポーネントとして実装

---
+++ components/UserLikesPage.vue
@@ -0,0 +1,86 @@
+<template>
+  <v-row justify="center">
+    <v-col cols="12" sm="8" md="7" lg="5">
+      <div v-for="like in likes" :key="like.id">
+        <v-hover v-slot:default="{ hover }">
+          <v-card class="mt-5 mb-9 pt-3 px-2 fadeUp" hover rounded nuxt :href="`/details/${like.post_uuid}`"
+            :class="hover ? 'user-transparent' : ''">
+            <!-- postの画像表示のためカードで枠組み -->
+            <v-container class="pb-0">
+              <v-card tile flat>
+                <!-- likeに紐づくpost_idと一致するpostの画像を表示 -->
+                <v-img :src="like.image_path" :lazy-src="like.image_path" aspect-ratio="1">
+                  <!-- ローディング中の処理 -->
+                  <template v-slot:placeholder>
+                    <v-row class="fill-height ma-0" align="center" justify="center">
+                      <v-progress-circular indeterminate color="primary"></v-progress-circular>
+                    </v-row>
+                  </template>
+                </v-img>
+              </v-card>
+            </v-container>
+            <v-card-text class="mt-0">
+              <p class="text-subtitle-1 text-center mb-1">{{ like.user.name }}さんの</p>
+              <p class="text-h6 text-center mb-1">{{ like.hash_tag }}</p>
+              <p class="text-subtitle-2 text-center mb-1">{{ like.created_at }}</p>
+            </v-card-text>
+          </v-card>
+        </v-hover>
+      </div>
+        <infinite-loading
+          @infinite="infiniteHandler"
+        ></infinite-loading>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import { axios } from '@/plugins/axios';
+import InfiniteLoading from 'vue-infinite-loading';
+
+export default {
+  components: {
+    InfiniteLoading,
+  },
+  props: {
+    likes: {
+      type: Array,
+      default: () => []
+    },
+    user: {
+      type: Object,
+      default: () => {}
+    },
+  },
+  data() {
+    return {
+      page: 1,
+    }
+  },
+  methods: {
+    async infiniteHandler($state) {
+      try {
+        this.page += 1;
+        const data = await axios.get(`users/${this.user.uid}/likes`, {
+          params: {
+            page: this.page,
+          },
+        });
+        setTimeout(() => {
+          if (data['data']['likes'].length) {
+            this.addLikes(data['data']['likes']);
+            $state.loaded();
+          } else {
+            $state.complete();
+          }
+        }, 1000);
+      } catch (error) {
+        $state.complete();
+      }
+    },
+    addLikes(newLikes) {
+      this.$emit('addLikes', newLikes);
+    },
+  },
+}
+</script>
---
+++ components/UserPostsPage.vue
@@ -0,0 +1,85 @@
+<template>
+  <v-row justify="center">
+    <v-col cols="12" sm="8" md="7" lg="5">
+      <div v-for="post in posts" :key="post.id">
+        <!-- post内容表示のためのにカードで大枠 -->
+        <v-hover v-slot:default="{ hover }">
+          <v-card class="mt-5 mb-9 pt-3 px-2 fadeUp" hover rounded nuxt :href="`/details/${post.post_uuid}`"
+            :class="hover ? 'user-transparent' : ''">
+            <!-- postの画像表示のためカードで枠組み -->
+            <v-container class="pb-0">
+              <v-card tile flat>
+                <v-img :src="post.image_path" :lazy-src="post.image_path" aspect-ratio="1">
+                  <!-- ローディング中の処理 -->
+                  <template v-slot:placeholder>
+                    <v-row class="fill-height ma-0" align="center" justify="center">
+                      <v-progress-circular indeterminate color="primary"></v-progress-circular>
+                    </v-row>
+                  </template>
+                </v-img>
+              </v-card>
+            </v-container>
+            <v-card-text class="mt-0">
+              <p class="text-h6 text-center mb-1">{{ post.hash_tag }}</p>
+              <p class="text-subtitle-2 text-center mb-1">{{ post.created_at }}</p>
+            </v-card-text>
+          </v-card>
+        </v-hover>
+      </div>
+        <infinite-loading
+          @infinite="infiniteHandler"
+        ></infinite-loading>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import { axios } from '@/plugins/axios';
+import InfiniteLoading from 'vue-infinite-loading';
+
+export default {
+  components: {
+    InfiniteLoading,
+  },
+  props: {
+    posts: {
+      type: Array,
+      default: () => []
+    },
+    user: {
+      type: Object,
+      default: () => {}
+    },
+  },
+  data() {
+    return {
+      page: 1,
+    }
+  },
+  methods: {
+    async infiniteHandler($state) {
+      try {
+        this.page += 1;
+        const data = await axios.get(`users/${this.user.uid}/posts`, {
+          params: {
+            page: this.page,
+          },
+        });
+        setTimeout(() => {
+          if (data['data']['posts'].length) {
+            this.addPosts(data['data']['posts']);
+            $state.loaded();
+          } else {
+            $state.complete();
+          }
+        }, 1000);
+      } catch (error) {
+        $state.complete();
+      }
+    },
+    addPosts(newPosts) {
+      this.$emit('addPosts', newPosts);
+    },
+  },
+}
+</script>
